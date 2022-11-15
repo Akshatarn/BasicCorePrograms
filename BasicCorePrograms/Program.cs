86b6d57577cf74a4d54cb914ad6e62eefcf72151
akshata_rn <akshatarn123@gamail.com>
2022-11-15 20:05:44 +0530

    Flip coin And Percentage for headcount and tail count

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -11,6 +11,27 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
+            Console.WriteLine("Flip Coin ");
+            Random random = new Random();
+            Console.WriteLine("Enter the number of times you want to flip coin");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int head = 0, tail = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double coinFlipped = random.NextDouble();
+                if (coinFlipped < 0.5)
+                {
+                    tail++;
+                }
+                else
+                {
+                    head++;
+                }
+            }
+            double headPercentage = head * 100 / n;
+            Console.WriteLine($"Head won {head} times, Head Score: {headPercentage}%");
+            double tailPercentage = tail * 100 / n;
+            Console.WriteLine($"Tail won {tail} times, Tail Score: {tailPercentage}%");
             Console.ReadLine();
         }
     }
