a22624215251b6e59dbe9fab6473409316a1401a
akshata_rn <akshatarn123@gamail.com>
2022-11-16 14:22:25 +0530

    Swapping Two Numbers

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -12,15 +12,18 @@ namespace BasicCorePrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
-            Console.WriteLine("Quotient And Remainder Program");
-            Console.WriteLine("Enter the number to be divided(divident):");
-            int divident = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the number should be divided from(Divisor):");
-            int divisor = Convert.ToInt32(Console.ReadLine());
-            int remainder = divident / divisor;
-            int quotient = divident % divisor;
-            Console.WriteLine("Remainder:" + remainder);
-            Console.WriteLine("Quotient:" + quotient);
+            Console.WriteLine("Swap Two Numbers Program");
+            Console.WriteLine("Enter the first number :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number :");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("a:" + a);
+            Console.WriteLine("b:" + b);
+            Console.ReadLine();
         }
     }
 }
